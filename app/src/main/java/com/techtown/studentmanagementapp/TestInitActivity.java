585ed871b1f7d307a63200dca9ec2950869b6d54
Baebin <baebine@naver.com>
2022-05-12 17:07:44 +0900

    Edited

---
+++ app/src/main/java/com/techtown/studentmanagementapp/TestInitActivity.java
@@ -0,0 +1,15 @@
+package com.techtown.studentmanagementapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TestInitActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test_init);
+
+    }
+}
