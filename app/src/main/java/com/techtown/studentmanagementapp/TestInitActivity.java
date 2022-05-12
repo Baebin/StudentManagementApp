f1366264932abd2826e5f5f7aab2755e22642ece
Baebin <baebine@naver.com>
2022-05-12 17:26:40 +0900

    Edited

--- app/src/main/java/com/techtown/studentmanagementapp/TestInitActivity.java
+++ app/src/main/java/com/techtown/studentmanagementapp/TestInitActivity.java
@@ -2,15 +2,34 @@ package com.techtown.studentmanagementapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class TestInitActivity extends AppCompatActivity {
     static public String TAG = "TestInitActivity";
+
+    private Button button_launch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test_init);
 
+        button_launch = findViewById(R.id.button_launch);
+        button_launch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sendIntent("Launch");
+            }
+        });
+    }
+
+    private void sendIntent(String activity) {
+        if (activity.equalsIgnoreCase("Launch")) {
+            Intent intent_launch = new Intent(getApplicationContext(), LaunchActivity.class);
+            startActivity(intent_launch);
+        }
     }
 }
