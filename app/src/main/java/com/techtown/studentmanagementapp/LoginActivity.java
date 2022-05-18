a5cf3ee309c777f1b3c3ca528d3955b9e8b37976
Baebin <baebine@naver.com>
2022-05-18 17:49:04 +0900

    Edited

--- app/src/main/java/com/techtown/studentmanagementapp/LoginActivity.java
+++ app/src/main/java/com/techtown/studentmanagementapp/LoginActivity.java
@@ -3,13 +3,36 @@ package com.techtown.studentmanagementapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class LoginActivity extends AppCompatActivity {
     static public String TAG = "LoginActivity";
+
+    private View view;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void showToast(String data) {
+        Log.d(TAG, "showToast(" + data + ")");
+        Toast.makeText(this, data, Toast.LENGTH_SHORT).show();
+    }
+
+    public void showSnackbar(String data) {
+        Log.d(TAG, "showSnackbar(" + data + ")");
+        final Snackbar snackbar = Snackbar.make(view, data, Snackbar.LENGTH_SHORT);
+        snackbar.setAction("확인", new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                snackbar.dismiss();
+            }
+        }).show();
     }
 }
