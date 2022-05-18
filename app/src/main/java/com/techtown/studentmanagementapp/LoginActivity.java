6ac4167f15e695ff76cf306dd12c43a3afebf072
Baebin <baebine@naver.com>
2022-05-18 17:59:40 +0900

    Edited

--- app/src/main/java/com/techtown/studentmanagementapp/LoginActivity.java
+++ app/src/main/java/com/techtown/studentmanagementapp/LoginActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.android.material.snackbar.Snackbar;
@@ -14,10 +16,32 @@ public class LoginActivity extends AppCompatActivity {
 
     private View view;
 
+    private EditText edtv_grade;
+    private EditText edtv_class;
+    private EditText edtv_number;
+    private EditText edtv_name;
+
+    private Button button_next;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        view = findViewById(R.id.view);
+
+        edtv_grade = findViewById(R.id.edtv_grade);
+        edtv_class = findViewById(R.id.edtv_class);
+        edtv_number = findViewById(R.id.edtv_number);
+        edtv_name = findViewById(R.id.edtv_name);
+
+        button_next = findViewById(R.id.button_next);
+        button_next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showSnackbar("Test");
+            }
+        });
     }
 
     public void showToast(String data) {
