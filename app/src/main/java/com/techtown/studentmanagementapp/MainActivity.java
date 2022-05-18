b14e3a176118eaf2557b53d7ee85a9bf6d904df9
Inchan <forlong4983@gmail.com>
2022-05-18 17:50:40 +0900

    Edited

--- app/src/main/java/com/techtown/studentmanagementapp/MainActivity.java
+++ app/src/main/java/com/techtown/studentmanagementapp/MainActivity.java
@@ -7,20 +7,10 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    private Button button_main;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        button_main = findViewById(R.id.button_main);
-        button_main.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-            }
-        });
-
-
     }
 }
