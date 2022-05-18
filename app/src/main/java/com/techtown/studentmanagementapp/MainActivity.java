c8af0775cdb23e72fc2c92c9ffb3b5bac3494b9e
Inchan <forlong4983@gmail.com>
2022-05-18 17:44:04 +0900

    Edited

--- app/src/main/java/com/techtown/studentmanagementapp/MainActivity.java
+++ app/src/main/java/com/techtown/studentmanagementapp/MainActivity.java
@@ -3,18 +3,23 @@ package com.techtown.studentmanagementapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button button_main;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Inchan Test
-        // Yewon Test
-        // Baebin Test
-        // Juheun Test
-        // Jiwoo Test
+        button_main = findViewById(R.id.button_main);
+        button_main.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            }
+        });
 
 
     }
