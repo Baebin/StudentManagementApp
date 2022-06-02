5d336af9be12811eee0eb6e13971008eb796ea60
Baebin <baebine@naver.com>
2022-06-02 17:31:37 +0900

    Edited

--- app/src/main/java/com/techtown/studentmanagementapp/MainActivity.java
+++ app/src/main/java/com/techtown/studentmanagementapp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.techtown.studentmanagementapp;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -7,13 +8,21 @@ import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.messaging.FirebaseMessaging;
 import com.techtown.studentmanagementapp.entity.Student;
+import com.techtown.studentmanagementapp.manager.FirebaseManager;
 import com.techtown.studentmanagementapp.util.SharedPreferenceUtil;
 
 public class MainActivity extends AppCompatActivity {
     static public String TAG = "MainActivity";
 
     public static Student student = null;
+    public static String token = "";
+
+    private FirebaseDatabase fdb;
 
     @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
@@ -28,5 +37,19 @@ public class MainActivity extends AppCompatActivity {
 
         Log.d(TAG, "Student Updated");
 
+        // Firebase
+        fdb = FirebaseDatabase.getInstance();
+        FirebaseManager.init(fdb);
+        FirebaseMessaging.getInstance().getToken().addOnCompleteListener(new OnCompleteListener<String>() {
+            @Override
+            public void onComplete(@NonNull Task<String> task) {
+                if (task.isComplete()) {
+                    token = task.getResult();
+                    Log.d(TAG, "Token: " + token);
+
+                    FirebaseManager.setToken(token);
+                } else Log.d(TAG, "Token: Error");
+            }
+        });
     }
 }
